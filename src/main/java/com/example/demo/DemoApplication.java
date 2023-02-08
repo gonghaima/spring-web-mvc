825a9109637b5a78d664d48274da8f81d0c76fe9
Steven Gong <gonghaima@hotmail.com>
2023-02-09 00:18:17 +1100

    add student data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.time.Month;
+import java.util.List;
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +20,18 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hell(){
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+				// new Student()
+				new Student(
+						// id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
+						// LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
+
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21));
 	}
 
 }
