29b9e24f50c9262d73ea60e4e56439d4a97c529a
Steven Gong <gonghaima@hotmail.com>
2023-02-09 20:55:44 +1100

    map request to api/v1/student

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,37 +1,12 @@
 package com.example.demo;
 
-import java.time.Month;
-import java.util.List;
-import java.time.LocalDate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.student.Student;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				// new Student()
-				new Student(
-						// id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
-						// LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
-
-						1L,
-						"Mariam",
-						"mariam.jamal@gmail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21));
 	}
 
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,3 +1,28 @@
 package com.example.demo.student;
 
-public class StudentController {}
+import java.time.Month;
+import java.util.List;
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> geStudents() {
+        return List.of(
+                // new Student()
+                new Student(
+                        // id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
+                        // LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
+
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21));
+    }
+}
