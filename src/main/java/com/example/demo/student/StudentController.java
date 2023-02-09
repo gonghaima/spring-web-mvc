9c213ba1a937f2cfe0f915deccc69496eb7d8d06
Steven Gong <gonghaima@hotmail.com>
2023-02-10 08:10:11 +1100

    move logic from controller to a service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -11,18 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> geStudents() {
-        return List.of(
-                // new Student()
-                new Student(
-                        // id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
-                        // LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
-
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21));
+        return studentService.geStudents();
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+import java.time.Month;
+import java.util.List;
+import java.time.LocalDate;
+
+public class StudentService {
+    public List<Student> geStudents() {
+        return List.of(
+                // new Student()
+                new Student(
+                        // id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
+                        // LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
+
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21));
+    }
+}
