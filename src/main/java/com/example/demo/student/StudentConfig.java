de10aacbcfaedb072853704ac25f43b9850df939
Steven Gong <gonghaima@hotmail.com>
2023-02-12 14:12:28 +1100

    data access to db

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Mariam = new Student(
+                    "Mariam",  
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21);
+            Student Alex = new Student(
+                    "Alex",
+                    "Alex @gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5),
+                    21);
+            repository.saveAll(List.of(Mariam, Alex));
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,24 +1,19 @@
 package com.example.demo.student;
-import java.time.Month;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-
 @Service
 public class StudentService {
-    public List<Student> geStudents() {
-        return List.of(
-                // new Student()
-                new Student(
-                        // id:1L, name:"Mariam", email:"mariam.jamal@gmail.com", dob:
-                        // LocalDate.of(year:2000, Month.JANUARY, dayOfMonth:5), age:21
+    private final StudentRepository studentRepository;
 
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21));
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> geStudents() {
+        return studentRepository.findAll();
     }
 }
