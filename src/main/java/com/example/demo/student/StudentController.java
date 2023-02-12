1b46d26b66d5abad524c8a144f2a2e5c95b01e4e
Steven Gong <gonghaima@hotmail.com>
2023-02-12 15:50:22 +1100

    post & save student

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,24 +1,32 @@
 package com.example.demo.student;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
 
     private final StudentService studentService;
 
-    @Autowired 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping
-    public List<Student> geStudents() {
-        return studentService.geStudents();
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerStudents(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
@@ -13,7 +16,19 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-    public List<Student> geStudents() {
+    public List<Student> getStudents() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        // return studentRepository.save()
+        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email t aken");
+        }
+        ;
+        studentRepository.save(student);
+    }
+
 }
